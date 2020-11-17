30fc7d3a82d2a52390241b3035dd8576de44ef4e
TheBreadEnergy <50107547+TheBreadEnergy@users.noreply.github.com>
2020-11-17 22:35:10 +0300

    beta v0.6

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,6 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
     creatIngredientBD();
     creatDishBD();
     setAll();
+    this->setWindowIcon(QIcon(":/images/logo.ico"));
 }
 
 MainWindow::~MainWindow()
--- window2.cpp
+++ window2.cpp
@@ -2,6 +2,7 @@
 #include "ui_window2.h"
 
 #include <QFile>
+#include <QIcon>
 #include <QVBoxLayout>
 
 window2::window2(QWidget *parent) :
@@ -9,6 +10,8 @@ window2::window2(QWidget *parent) :
     ui(new Ui::window2)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Рецепт");
+    this->setWindowIcon(QIcon(":/images/logo.ico"));
 }
 
 window2::~window2()
@@ -24,36 +27,22 @@ void window2::broadcast(int id, QVector<dish> dishes){
 
     QString str = file.readAll();
 
+    int d = 1;
     for(int i = 0; i < str.length(); i++){
-        if(str[i] != "@"){
-            param1 += str[i];
-        }else{
-            break;
-        }
-    }
-
-    for(int i = 0; i < str.length(); i++){
-        if(str[i] != "@"){
-            param2 += str[i];
-        }else{
-            break;
-        }
-    }
-
-
-    for(int i = 0; i < str.length(); i++){
-        if(str[i] != "@"){
-            param3 += str[i];
-        }else{
-            break;
-        }
-    }
-
-    for(int i = 0; i < str.length(); i++){
-        if(str[i] != "@"){
-            param4 += str[i];
-        }else{
-            break;
+        switch (d) {
+        case 1: if(str[i] == "@") d++;
+                 else param1 += str[i];
+                 break;
+        case 2: if(str[i] == "@") d++;
+                else param2 += str[i];
+                break;
+        case 3: if(str[i] == "@") d++;
+                else param3 += str[i];
+                break;
+        case 4: if(str[i] == "@") d++;
+                else param4 += str[i];
+                break;
+        default:break;
         }
     }
 
@@ -68,9 +57,9 @@ void window2::broadcast(int id, QVector<dish> dishes){
     case 2:
         ui->label->setPixmap(QPixmap(":/images/myaso-po-francuzski.jpg"));
         ui->label_2->setText(dishes[1].getName());
-        ui->label_3->setText(param2);
-        ui->label_4->setText("Килокалорий в 100гр продукта: " + QString::number(dishes[1].getCcal() / 10));
-        ui->label_5->setText("Белки: " + QString::number(dishes[1].getProtein()) + " " + "Жиры: " + QString::number(dishes[1].getFats()) + " " + "Углеводы: " + QString::number(dishes[1].getCarbohydrates()));
+        ui->label_3->setText("Килокалорий в 100гр продукта: " + QString::number(dishes[1].getCcal() / 10));
+        ui->label_4->setText("Белки: " + QString::number(dishes[1].getProtein()) + " " + "Жиры: " + QString::number(dishes[1].getFats()) + " " + "Углеводы: " + QString::number(dishes[1].getCarbohydrates()));
+        ui->label_5->setText(param2);
         break;
     case 3:
         ui->label->setPixmap(QPixmap(":/images/vegetable_stew.jpg"));
